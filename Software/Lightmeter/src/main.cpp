58e91aae82464b4d645902a92046a39c5b8bd132
Fredestroyer007 <fredlarochelle@outlook.com>
2020-12-26 02:34:10 -0500

    MVP

--- Software/Lightmeter/src/main.cpp
+++ Software/Lightmeter/src/main.cpp
@@ -5,39 +5,51 @@
 
 Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591);
 
+const double CALIBRATION_INCIDENT = 250;
+
 bool gotReading, newReadingGain;
-uint8_t gainSettingTemp, gainSetting; 
-float lux, ev;
+uint8_t gainSetting; 
+double lux, ev;
 double hysteresis = 1;
 
+double fStop = 1;
+double shutterSpeed = 0.008;
+double iso = 2500;
+
+
 // Different sensitivity settings for the TSL2591 luminosity sensor
-void configureSensor(int choice) 
+void configureSensor(uint16_t gain) 
 {
-Serial.println(F("Config sensor!!!"));
-  if (choice == 0) 
+
+  if (gain == 0) 
   { // Least sensitivity possible
     tsl.setGain(TSL2591_GAIN_LOW);    // 1x gain
     tsl.setTiming(TSL2591_INTEGRATIONTIME_100MS);   // shortest integration time
   } 
-  else if (choice == 1) 
+  else if (gain == 1) 
   {  
     tsl.setGain(TSL2591_GAIN_LOW);    // 1x gain  
-    tsl.setTiming(TSL2591_INTEGRATIONTIME_600MS);   // longest integration time
+    tsl.setTiming(TSL2591_INTEGRATIONTIME_400MS); 
     hysteresis = 0.8;
   } 
-  else if (choice == 2) 
+  else if (gain == 2) 
   {  
     tsl.setGain(TSL2591_GAIN_MED);    // 25x gain
-    tsl.setTiming(TSL2591_INTEGRATIONTIME_600MS);   // longest integration time   
+    tsl.setTiming(TSL2591_INTEGRATIONTIME_400MS);   
     hysteresis = 0.9;
   } 
-  else if (choice == 3) 
-  { // Highest sensitivity settings
+  else if (gain == 3) 
+  {
     tsl.setGain(TSL2591_GAIN_HIGH);   // 428x gain
-    tsl.setTiming(TSL2591_INTEGRATIONTIME_600MS);   // longest integration time   
+    tsl.setTiming(TSL2591_INTEGRATIONTIME_400MS); 
     hysteresis = 1.1;
   }
-
+  else if (gain == 4) 
+  {
+    tsl.setGain(TSL2591_GAIN_MAX); 
+    tsl.setTiming(TSL2591_INTEGRATIONTIME_600MS);   
+    hysteresis = 1.1;
+  }
   newReadingGain = true;  // The next measurement will be less accurate, it shouldn't be used.
 }
 
@@ -63,52 +75,94 @@ void setup(void)
 }
 
 // Convert lux to ev, only valid for 100 ASA
-float lux2ev(float lux) 
+double lux2ev(double lux) 
 {
 	return (log(lux) - log(2.5)) / log(2);
+}
+
+/* Calculate the ISO
+   https://www.wolframalpha.com/input/?i=solve+s+in+%28n**2%2Ft%29+%3D+%28%28e*s%29%2Fc%29
+*/
+double calculateIso()
+{
+  double calculateIsoTemp = (CALIBRATION_INCIDENT * (pow(fStop, 2))) / (lux * shutterSpeed);
+
+  return calculateIsoTemp;
+}
+
+/* Calculate the shutter speed
+   https://www.wolframalpha.com/input/?i=solve+t+in+%28n**2%2Ft%29+%3D+%28%28e*s%29%2Fc%29
+*/
+double calculateShutterSpeed()
+{
+  double shutterSpeedTemp = (CALIBRATION_INCIDENT * (pow(fStop, 2))) / (lux * iso);
+
+  return shutterSpeedTemp;
+}
+
+/* Calculate the shutter speed
+   https://www.wolframalpha.com/input/?i=solve+n+in+%28n**2%2Ft%29+%3D+%28%28e*s%29%2Fc%29
+*/
+double calculateFStop()
+{
+  double fStopTemp = (sqrt(lux) * sqrt(iso) * sqrt(shutterSpeed)) / sqrt(CALIBRATION_INCIDENT);
+
+  return fStopTemp;
+}
+
+/* Solve the incident constant
+   https://www.wolframalpha.com/input/?i=solve+c+in+%28n**2%2Ft%29+%3D+%28%28e*s%29%2Fc%29
+*/
+double solveIncidentConstant()
+{
+  double incidentConstant = (lux * iso * shutterSpeed) / (pow(fStop, 2));
+
+  return incidentConstant;
 }
 
 /* TSL2591 Autogain based on code by D-Winker :
    https://github.com/D-Winker/TSL2591_AutoAdjust
 */
-float sensorReading() 
+double sensorReading() 
 {
-  Serial.println(F("Begin Reading!!!"));
-  float newLux;
+  uint16_t ir, full;
+  uint8_t gainSettingTemp;
+  double newLux;
 
-  while (!gotReading) 
+  while(!gotReading) 
   {
-    if (newReadingGain) 
-    { // We just changed the gain settings, so we need to take a reading then throw it out.
+    if(newReadingGain) 
+    {
       tsl.getFullLuminosity();
       newReadingGain = false;
-      Serial.println(F("Carbage Reading"));
-    } 
-    else 
-    { // If the gain wasn't just changed.
-    Serial.println(F("If the gain wasnt changed"));
+    }
+    else
+    {
+      uint32_t lum = tsl.getFullLuminosity();
+      ir = lum >> 16;
+      full = lum & 0xFFFF;
       gainSettingTemp = gainSetting;
 
-      // Take a measurement
-      uint32_t lum = tsl.getFullLuminosity();  // Read 32 bits with top 16 bits IR, bottom 16 bits full spectrum.    
-      newLux = tsl.calculateLux((lum & 0xFFFF), (lum >> 16));
-      
-      // Adjust the gain if necessary
-      if (newLux == 0 || newLux > 60000) 
+      newLux = tsl.calculateLux(full, ir);
+      // Interpret the measurement and decide if the gain should be adjusted
+      if (newLux <= 0 || newLux > 60000) 
       {  // The sensor saturated or is close
-        if (gainSetting > 0) 
+        if(gainSettingTemp != 0)
         {
-          gainSettingTemp = gainSetting - 1;  // Decrease the gain
+          gainSettingTemp--;
         }
       } 
       else if (newLux > 1700 * hysteresis) 
       {
         gainSettingTemp = 1;  // Use low gain setting
-      } 
-      else if (newLux < 100 * hysteresis) 
-      {
+      }  
+      else if (newLux < 100 * hysteresis && newLux > 5 * hysteresis) {
         gainSettingTemp = 3;  // Use high gain setting
-      } 
+      }
+      else if (newLux < 5 * hysteresis) 
+      {
+        gainSettingTemp = 4;  // Use max gain setting
+      }
       else 
       {
         gainSettingTemp = 2;  // Use normal gain setting
@@ -117,32 +171,30 @@ float sensorReading()
       // Adjust the gain or set the measurements as appropriate
       if (gainSetting == gainSettingTemp) 
       {  // Nothing has changed
-        if (newLux < 0) 
-        {
-          newLux = 0;
-        }
-
         gotReading = true;
-        return newLux;
       } 
       else 
       {
         Serial.print("Updating setting "); Serial.print(gainSetting); Serial.print(" to "); Serial.println(gainSettingTemp);
-        gainSetting = gainSettingTemp;
+        if(gainSettingTemp <= 4 && gainSettingTemp >= 0) {
+          gainSetting = gainSettingTemp;
+        }
         configureSensor(gainSetting);
       }
     }
   }
-
   gotReading = false;
+  return newLux;
 }
 
 void loop(void) { 
   lux = sensorReading();
   ev = lux2ev(lux);
 
-  Serial.print(F("Lux: ")); Serial.println(lux, 6);
-  Serial.print(F("EV: ")); Serial.println(ev, 6);
-  
-  delay(2000);
+  Serial.print(F("Lux: ")); Serial.println(lux);
+  Serial.print(F("EV: ")); Serial.println(ev);
+
+  Serial.print(F("ISO: ")); Serial.println(calculateIso());
+
+  delay(500);
 }
